a518813f8a8df16ad2246cb0429054caf6ea9e3d
Takuya Matsuyama <hi@craftz.dog>
2024-04-26 16:35:14 +0900

    fix(tailwind): use @apply instead of rehype-add-class

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, sharpImageService } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,31 +14,6 @@ export default defineConfig({
     shikiConfig: {
       theme: 'dark-plus'
     },
-    extendDefaultPlugins: true,
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: 'text-4xl font-bold font-mplus my-2',
-          h2: 'text-2xl font-bold font-mplus my-2',
-          h3: 'text-xl font-bold font-mplus my-2',
-          h4: 'text-lg font-bold font-mplus my-2',
-          h5: 'font-bold font-mplus my-2',
-          h6: 'font-bold font-mplus my-2',
-          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
-          p: 'mb-6',
-          a: 'underline underline-offset-2 text-indigo-600 dark:text-indigo-400 decoration-indigo-400 dark:decoration-indigo-600',
-          ul: 'list-disc list-outside',
-          ol: 'list-decimal list-outside',
-          table:
-            'table-auto border border-slate-200 dark:border-zinc-700 rounded-lg',
-          th: 'border dark:border-zinc-700 font-bold p-4 pb-3 text-left',
-          td: 'border border-slate-200 dark:border-zinc-700 p-4',
-          hr: 'border-slate-200 dark:border-zinc-500 my-4',
-          blockquote:
-            'px-4 my-4 border-l-4 border-l-slate-200 dark:border-l-zinc-500 italic'
-        }
-      ]
-    ]
+    extendDefaultPlugins: true
   }
 })
